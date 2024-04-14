65562630eb8229eb116629d55fc8db83e3bd5006
BriceBZH <brice.rubeaux@wanadoo.fr>
2024-04-14 17:21:14 +0200

    made modal for formats of album

--- assets/scripts/cards.js
+++ assets/scripts/cards.js
@@ -4,28 +4,24 @@ document.addEventListener("DOMContentLoaded", function() {
     cards.forEach(function(card) {
         const art = card.firstChild.nextSibling;
         const imageUrl = card.dataset.imageUrl;
-        // card.style.backgroundColor = "rgba(0,0,0,0.5)";
         card.style.background = `url('${imageUrl}') no-repeat`;
         card.style.backgroundSize = "cover";
-        // art.style.marginTop = "-40%";
     });
+
     //selection du format de l'album de la collection
-    document.addEventListener("click", function(e){
-        const paragraphs = document.querySelectorAll('.formats');
-        paragraphs.forEach(function(paragraph) {
-            paragraph.style.display = 'none';
-        });
-        let element = e.target;
-        let target = element.dataset.target;
-        if (target) {
-            const el = document.querySelector("." + target);
-            if (el) {
-                if (el.style.display === 'block') {
-                    el.style.display = 'none';
-                } else {
-                    el.style.display = 'block';
-                }
-            }
-        }
-    });
+    const modalContainer = document.querySelector(".modal-container");
+    const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+    modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+
+    function toggleModal() {
+        modalContainer.classList.toggle("active");
+        // Récupérer le contenu du paragraphe
+        const formatsParagraph = this.parentElement.querySelector(".formats");
+        const formatsContent = formatsParagraph.innerHTML;
+
+        // Mettre à jour le contenu de la fenêtre modale avec le contenu du paragraphe
+        const modalContent = document.querySelector(".modal-content");
+        modalContent.innerHTML = formatsContent;
+    }
 });
