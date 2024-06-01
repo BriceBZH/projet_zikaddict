07e053cd273d1216b9cac3f9e60726ac45e9753a
BriceBZH <brice.rubeaux@wanadoo.fr>
2024-06-01 21:29:57 +0200

    bug fixed modal window

--- assets/scripts/cards.js
+++ assets/scripts/cards.js
@@ -10,30 +10,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //selection du format de l'album de la collection
     const modalContainer = document.querySelector(".modal-container");
-    const closeModal = document.querySelector(".close-modal");
     const modalTriggers = document.querySelectorAll(".modal-trigger");
 
     modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
-    if(closeModal) {
-        closeModal.addEventListener("click", closeModalHandler);
 
-        function toggleModal() {
-            modalContainer.classList.toggle("active");
-            // Récupérer le contenu du paragraphe
-            const formatsParagraph = this.parentElement.querySelector(".formats");
-            const formatsContent = formatsParagraph.innerHTML;
+    function toggleModal() {
+        modalContainer.classList.toggle("active");
+        // Récupérer le contenu du paragraphe
+        const formatsParagraph = this.closest("li").querySelector(".formats");
+        const formatsContent = formatsParagraph.innerHTML;
 
-            // Mettre à jour le contenu de la fenêtre modale avec le contenu du paragraphe
-            const modalContent = document.querySelector(".modal-content");
-            modalContent.innerHTML = formatsContent + '<button class="close-modal">X</button>';
+        // Mettre à jour le contenu de la fenêtre modale avec le contenu du paragraphe
+        const modalContent = document.querySelector(".modal-content");
+        modalContent.innerHTML = formatsContent + '<button class="close-modal">X</button>';
 
         // Ajouter un gestionnaire d'événement pour le bouton de fermeture
         const closeButton = modalContent.querySelector(".close-modal");
         closeButton.addEventListener("click", closeModalHandler);
-        }
+    }
 
-        function closeModalHandler() {
-            modalContainer.classList.remove("active");
-        }
+    function closeModalHandler() {
+        modalContainer.classList.remove("active");
     }
 });
--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     let nav = document.getElementById("nav");
     icons.setAttribute("data-content", "☰");
     icons.addEventListener("click", () => {
-        nav.classList.toggle("active");
-        if (document.querySelector("nav").classList.contains("active")) {
-            // Si la classe active est présente, changez le contenu de l'icône à "✕"
+        nav.classList.toggle("on");
+        if (document.querySelector("nav").classList.contains("on")) {
+            // Si la classe on est présente, changez le contenu de l'icône à "✕"
             icons.setAttribute("data-content", "✕");
         } else {
-            // Si la classe active n'est pas présente, changez le contenu de l'icône à "☰"
+            // Si la classe on n'est pas présente, changez le contenu de l'icône à "☰"
             icons.setAttribute("data-content", "☰");
         }
     })
