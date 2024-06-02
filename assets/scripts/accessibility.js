c637472b2c84bcafb0c90aab09844da000b56cd9
BriceBZH <brice.rubeaux@wanadoo.fr>
2024-06-02 12:30:23 +0200

    add accessibility tools

---
+++ assets/scripts/accessibility.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const showTools = document.getElementById('tools');
+    const tools = document.getElementById('accessibility-tools');
+
+    showTools.addEventListener('click', () => {
+        if (tools.style.display === 'none' || tools.style.display === '') {
+            tools.style.display = 'block';
+        } else {
+            tools.style.display = 'none';
+        }
+    });
+
+    const increaseText = document.getElementById('increase_text_size');
+    const decreaseText = document.getElementById('decrease_text_size');
+    const darkMode = document.getElementById('dark_mode');
+    const body = document.body;
+    let currentFontSize = parseFloat(window.getComputedStyle(body, null).getPropertyValue('font-size'));
+
+    increaseText.addEventListener('click', () => {
+        currentFontSize += 1;
+        body.style.fontSize = currentFontSize + 'px';
+    });
+
+    decreaseText.addEventListener('click', () => {
+        currentFontSize -= 1;
+        body.style.fontSize = currentFontSize + 'px';
+    });
+
+    darkMode.addEventListener('click', () => {
+        body.classList.toggle('dark_mode');
+    });
+});
